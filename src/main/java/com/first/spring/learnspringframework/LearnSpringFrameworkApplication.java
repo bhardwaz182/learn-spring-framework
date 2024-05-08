14d4a23d9a4c4909f2a0fad8075dd06a266392a9
Bhardwaz <bharadwaz182@gmail.com>
2024-05-08 15:23:05 -0400

    tightly coupled application

--- src/main/java/com/first/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/first/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -3,11 +3,19 @@ package com.first.spring.learnspringframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.first.spring.learnspringframework.game.GameRunner;
+import com.first.spring.learnspringframework.game.MarioGame;
+import com.first.spring.learnspringframework.game.SuperContraGame;
+
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+//		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+//		MarioGame mario = new MarioGame();
+		SuperContraGame superContra = new SuperContraGame();
+		GameRunner runner = new GameRunner(superContra);
+		runner.run();
 	}
 
 }
---
+++ src/main/java/com/first/spring/learnspringframework/game/GameRunner.java
@@ -0,0 +1,16 @@
+package com.first.spring.learnspringframework.game;
+
+public class GameRunner {
+	private SuperContraGame game;
+	
+	public GameRunner(SuperContraGame game) {
+		this.game=game;
+	}
+	
+	public void run() {
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+	}
+}
